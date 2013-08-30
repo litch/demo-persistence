40e2ad786ecdcfb2db2dbfb2abab2e5651fba635
Justin Litchfield <litch@me.com>
2013-08-29 23:45:43 -0500

    first cobblings coming together

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'rails', '4.0.0'
 
 gem 'rails-api'
 
-
+gem "mongoid"
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
@@ -14,10 +14,6 @@ gem 'rails-api'
 # gem 'jbuilder'
 
 # Use unicorn as the app server
-# gem 'unicorn'
+gem 'unicorn'
 
-# Deploy with Capistrano
-# gem 'capistrano', :group => :development
-
-# To use debugger
-# gem 'ruby-debug19', :require => 'ruby-debug'
+gem 'rspec'
--- config/application.rb
+++ config/application.rb
@@ -7,10 +7,6 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
-
 module DemoPersistence
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -19,7 +15,7 @@ module DemoPersistence
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
